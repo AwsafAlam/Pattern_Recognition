d975c26e34a7615569e05e1af7779928da560e31
Md Awsaf Alam <awsafalam@gmail.com>
2020-09-18 20:47:00 +0600

    perceptron pseudocode

--- Perceptron/Online-19.09.2020/main.py
+++ Perceptron/Online-19.09.2020/main.py
@@ -1,9 +1,9 @@
 import numpy as np
 import math
 
-number_of_features = 0
-number_of_classes = 0
-dataset_size = 0
+Features = 0
+Classes = 0
+TrainingSize = 0
 Object_Dictionary = {}
 correct = 0
 
@@ -31,14 +31,14 @@ class Object:
         return np.std(temp)
 
     def calc_mean(self):
-        global number_of_features
-        for i in range(number_of_features):
+        global Features
+        for i in range(Features):
             mu = self.get_mean(i)
             self.mean.append(mu)
 
     def calc_standard_deviation(self):
-        global number_of_features
-        for i in range(number_of_features):
+        global Features
+        for i in range(Features):
             sigma = self.get_standard_deviation(i)
             self.sd.append(sigma)
 
@@ -52,29 +52,37 @@ class Object:
 
 
 def read_dataset():
-    global number_of_features, number_of_classes, dataset_size, Object_Dictionary
+    global Features, Classes, TrainingSize, Object_Dictionary
 
     f = open("./train.txt", "r")
     lines = f.readlines()
     f.close()
 
-    number_of_features, number_of_classes, dataset_size = map(int, lines[0].split())
+    Features, Classes, TrainingSize = map(int, lines[0].split())
 
-    for i in range(dataset_size):
+    for i in range(TrainingSize):
         data = lines[i + 1].split()
         # print(data)
-        class_name = data[number_of_features] # last element of data array
+        class_name = data[Features] # last element of data array
 
         if class_name not in Object_Dictionary: # saving features for each distinct classes
             Object_Dictionary[class_name] = Object(class_name)
 
-        Object_Dictionary[class_name].features.append(data[: number_of_features])
+        Object_Dictionary[class_name].features.append(data[: Features])
 
     # print(Object_Dictionary)
 
 
 def train():
-    global number_of_classes, number_of_features, Object_Dictionary, dataset_size
+    global Classes, Features, Object_Dictionary, TrainingSize
+  # 1. Initialize w' with random number length of (number of features + 1)
+
+  # 2. let l = learning rate set to a fixed value.
+
+  # 3. set t = 0
+
+  # 4. while misclassified_set is not empty: misclassified <- {} for k <- len(training_data): vector_class = get_class(training_data[k]) discreminent = dot_product(w', training_data[k]) if vector_class is misclassified: misclassified_set <- training_data[k] cost = cost_function(misclassified_set) w' = w - l*cost
+  # Here cost_function = summation of all misclassified vectors with their respective
 
     for key in Object_Dictionary: # obtain mean and std. store in the object itself
         obj = Object_Dictionary[key]
@@ -84,7 +92,7 @@ def train():
 
 
 def test_accuracy():
-    global number_of_features, Object_Dictionary, dataset_size, correct
+    global Features, Object_Dictionary, TrainingSize, correct
 
     f = open("./test.txt", "r")
     lines = f.readlines()
@@ -105,56 +113,11 @@ def test_accuracy():
             if len(t):
                 test_vector.append(float(t))
 
-        class_name = temp[number_of_features]
+        class_name = temp[Features]
         print(test_vector,class_name)
-        
-        # max = 0
-        # predicted_class = ''
-        # for key in Object_Dictionary:
-        #     obj = Object_Dictionary[key]
-        #     prior_prob = len(obj.features) / dataset_size # p ( ci )
-
-        #     co_var = []
-        #     # Finding co-variant matrix
-        #     for i in range(number_of_features):
-        #         tmp = arr[i] - obj.mean[i]
-        #         co_var.append(tmp)
-            
-        #     co_var_array = np.array([i for i in co_var]).astype(float)
-        #     # co_var_array = np.array(co_var)
-        #     co_var_array = co_var_array.reshape(-1,co_var_array.size)
-        #     trans = co_var_array.reshape(co_var_array.size, -1)
-        #     sigma = np.dot(co_var_array,trans) # sigma is the new 3 x 3 arr
-        #     sigma_inv = np.linalg.inv(sigma) 
-        #     det = np.linalg.det(sigma)
-        #     d = sigma.shape[0]
-
-        #     # print(co_var_array , co_var_array.shape, d) 
-        #     # print(trans , trans.shape)
-        #     # print("----------\n",sigma, sigma.shape,"\n______")
-
-        #     for i in range(number_of_features):
-        #         # p(c)p(Fi | c) for all features
-        #         # prior_prob *= get_prob_f_given_c(arr[i] , obj.sd[i], obj.mean[i])
-        #         # get_prob(arr[i])
-        #         x_minus_mu = arr[i] - obj.mean[i]
-        #         ex = math.exp(-0.5 * x_minus_mu *sigma_inv[0][0] * x_minus_mu)
-        #         tmp = 1 / (2 * math.pi *math.pow(det, 0.5))
-        #         prior_prob *= (tmp * ex)
-        #         # print("Prob : "+str(tmp * ex))
-
-        #     if max <= prior_prob:
-        #         predicted_class = obj.class_name
-        #         max = prior_prob
-
-        # if predicted_class == class_name:
-        #     correct += 1
-        # else:
-        #     print("sample no: " + str(sample) + ", feat:" + str(arr[: number_of_features]) \
-        #              + ", actual-class:" + str(class_name) + ", predicted-class: " + str(predicted_class) + "\n")
-
-    acc = (correct / float(dataset_size)) * 100.0
-    print("accuracy: {} / {} = {}%".format(correct,dataset_size,acc))
+    
+    # acc = (correct / float(TrainingSize)) * 100.0
+    # print("accuracy: {} / {} = {}%".format(correct,TrainingSize,acc))
     # wr.close()
 
 
