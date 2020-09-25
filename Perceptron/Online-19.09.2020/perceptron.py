a128bd7fa3649bc3698f69ae689c20a6fb294da6
Md Awsaf Alam <awsafalam@gmail.com>
2020-09-25 12:05:09 +0600

    REWARD:PUNISH- DONE

--- Perceptron/Online-19.09.2020/perceptron.py
+++ Perceptron/Online-19.09.2020/perceptron.py
@@ -2,8 +2,9 @@ import numpy as np
 
 class Perceptron(object):
 
-    def __init__(self, features, threshold=100, learning_rate=0.01):
+    def __init__(self, features, threshold=500, learning_rate=0.01):
         np.random.seed(23)
+        self.features = features
         self.threshold = threshold
         self.learning_rate = float(learning_rate)
         self.bias = 0
@@ -24,26 +25,33 @@ class Perceptron(object):
     def train(self, training_inputs, labels):
         for i in range(self.threshold):
           print("Iteration {} ==========".format(i))
-          misclassified = 0
+          misclassified = []
           for inputs, label in zip(training_inputs, labels):
             prediction = self.predict(inputs) # output of unit activation function
             # print(inputs, label, prediction)
             ## Reward punish -- ?
             if label == 1 and prediction == 0: # class1 misclassified.
               # Wi = Wi + n*d*input -> d = 1 or -1.
-              misclassified += 1
-              self.weights[1:] += self.learning_rate * inputs * 1
-              self.weights[0] += self.learning_rate # changing the bias
+              misclassified.append(inputs)
 
             elif label == 2 and prediction == 1: # class2 misclassified
-              misclassified += 1
-              self.weights[1:] += self.learning_rate * inputs * -1
-              self.weights[0] += self.learning_rate * -1
+              misclassified.append(inputs * -1)
             else:
               # print("Converging --")
               pass # do nothing
           
-          print(misclassified)
-            ## Basic perceptron
-            # self.weights[1:] += self.learning_rate * (label - prediction) * inputs
-            # self.weights[0] += self.learning_rate * (label - prediction)
+          print("Misclassified: {} => ".format(len(misclassified)))
+          if len(misclassified) == 0:
+            break
+          sum = np.zeros(self.features)
+          for i in range(len(misclassified)):
+            sum += misclassified[i]
+          self.weights[1:] -= self.learning_rate * sum
+          print(self.weights)
+          # self.weights[0] += self.learning_rate # changing the bias
+
+          # self.weights[1:] += self.learning_rate * inputs * 1
+          # self.weights[0] += self.learning_rate # changing the bias
+          ## Basic perceptron
+          # self.weights[1:] += self.learning_rate * (label - prediction) * inputs
+          # self.weights[0] += self.learning_rate * (label - prediction)
