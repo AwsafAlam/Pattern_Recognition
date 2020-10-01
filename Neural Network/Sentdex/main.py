7754a29f392f93c37b9f4f956ba5f05cb2985c2b
Md Awsaf Alam <awsafalam@gmail.com>
2020-10-01 15:25:15 +0600

    NN: single layer

---
+++ Neural Network/Sentdex/main.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+inputs = [1,2,3, 2.5]
+weights1 = [0.2,0.8,-0.5, 1.0]
+weights2 = [0.5, -0.91, 0.26, -0.5]
+weights3 = [-0.26, -0.27,0.17, 0.87]
+
+bias1 = 2.0
+bias2 = 3.0
+bias3 = 0.5
+
+single_neuron = np.dot(inputs, weights1) + bias1
+
+layer = [np.dot(inputs, weights1) + bias1,
+          np.dot(inputs, weights2) + bias2,
+          np.dot(inputs, weights3) + bias3
+          ]
+
+print(layer)
