3825c74e5e24cd4e82b663a302851ee875093167
Md Awsaf Alam <awsafalam@gmail.com>
2020-10-03 03:29:44 +0600

    Added offline spec

---
+++ Neural Network/Offline/main.py
@@ -0,0 +1,19 @@
+# single layer network
+
+# Add a Layer class
+# Create an instance for eah layer, and take the structure defined in the text file
+
+# weights will be hard coded for now
+
+# There will be 4 activation algo.
+# 1. Sigmoid 2, unit step func. 3. RELU 4. softmax 5. study other literature
+
+# create a forward pass, 64 samples batch, and train the network
+# repeat until all the training samples are exhausted.
+
+# test using the test file and calculate the accuracy for hard-coded weights and bias.
+
+# --------- 
+
+# calculate the error rate, and based on that, implement the gradient descent algo.
+# using the gradient descent, adjust the weights and bias to get maximum fit of data.
