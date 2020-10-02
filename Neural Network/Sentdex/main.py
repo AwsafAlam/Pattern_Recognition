908e7e7200d6d23feaf814cfea84774acd319a5b
Md Awsaf Alam <awsafalam@gmail.com>
2020-10-03 02:39:55 +0600

    Added activation functioln

--- Neural Network/Sentdex/main.py
+++ Neural Network/Sentdex/main.py
@@ -1,38 +1,42 @@
-import numpy as np
+import numpy as np 
+# import nnfs
 
-np.random.seed(0)
+#https://cs231n.github.io/neural-networks-case-study/
+def spiral_data(points, classes):
+    X = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(class_number+1))
+        r = np.linspace(0.0, 1, points)  # radius
+        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
+# from nnfs.datasets import spiral_data  # See for code: https://gist.github.com/Sentdex/454cb20ec5acf0e76ee8ab8448e6266c
 
-## We take in a batch of inputs
-X = [[1,2, 3, 2.5],
-    [2.0, 5.0, -1.0, 2.0],
-    [-1.5,2.7, 3.3, -0.8]]
+# nnfs.init()
 
-class Layer_dense:
-  """
-  docstring
-  """
-  def __init__(self, n_inputs, n_neurons):
-    """
-    docstring
-    """
-    # Gausian distribution bounded around 0
-    self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
-    self.biases = np.zeros((1, n_neurons))
+X, y = spiral_data(100, 3)   
 
-  
-  def forward(self, inputs):
-    """
-    docstring
-    """
-    self.output = np.dot(inputs, self.weights) + self.biases
 
-# 4 features, and 5 neurons
-layer1 = Layer_dense(4, 5)
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-# 5 putputs of l1, and 2 neurons
-layer2 = Layer_dense(5, 2)
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+layer1 = Layer_Dense(2,5)
+activation1 = Activation_ReLU()
 
 layer1.forward(X)
-print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+
+#print(layer1.output)
+activation1.forward(layer1.output)
+print(activation1.output)
