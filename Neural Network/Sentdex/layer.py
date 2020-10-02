43642b26228253c482f89f278d7bd6abe045cdae
Md Awsaf Alam <awsafalam@gmail.com>
2020-10-02 20:09:32 +0600

    Added multi layer

---
+++ Neural Network/Sentdex/layer.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+inputs = [1,2,3, 2.5]
+weights = [[0.2,0.8,-0.5, 1.0],
+            [0.5, -0.91, 0.26, -0.5],
+            [-0.26, -0.27,0.17, 0.87]]
+
+bias = [2.0, 3.0, 0.5]
+
+# output_layer = []
+# for i in range(len(weights)):
+#   l = np.dot(inputs, weights[i]) + bias[i]  
+#   output_layer.append(l)
+
+output_layer = np.dot(weights, inputs) + bias
+
+## inputs cannot change, but we can tweak the weights and bias.
+# the way we tweak the W & B, using back propagation allows us to have a very good neural net.
+
+print(output_layer)
