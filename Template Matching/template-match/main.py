d1fded1e41c32d539ab4cceae3b4b40aa97e4eec
Md Awsaf Alam <awsafalam@gmail.com>
2020-10-30 22:16:39 +0600

    logging

--- Template Matching/template-match/main.py
+++ Template Matching/template-match/main.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 
+f = open("output.txt", "w")
+f.write("Starting template matching")
+
 img_rgb = cv2.imread('test.jpg')
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
@@ -20,25 +23,24 @@ def calculate_distance(row = 0, col = 0):
   """
   calculate the relative difference
   """
-  print("Started matching [{},{}]".format(row, col))
-  sum = 0
+  f.write("Started matching [{},{}]\n".format(row, col))
+  sum, diff = 0 , 0
   for i in range(h):
     for j in range(w):
-      diff = template[i][j] - img_gray[row + i][col + j]
-      sum = sum + diff * diff
+      diff = int(template[i][j]) - int(img_gray[row + i][col + j])
+      sum = sum + pow(int(diff), 2)
       if diff != 0:
-        print("Diff {}: [{},{}]".format(diff,i,j))
+        f.write("Diff {}: [{},{}] ".format(diff,i,j))
+    f.write("\n\n-------------------\n\n")
+  f.write("--------------------------------\n\n")
+  return int(sum)
 
-  print("--------------------------------")
-  return sum
-
-print(calculate_distance(0,0))
+f.write(str(calculate_distance(0,0)))
 
 for i in range(test_height - h):
   for j in range(test_width - w):
-    pass
     # calculating the mean distance
-    # print(calculate_distance(i,j))
+    min = calculate_distance(i,j)
     # if int(img_gray[i][j]) != 255:
     #   # print(img_gray[i][j])
     #   x_axis.append(i)
@@ -55,3 +57,5 @@ for pt in zip(*loc[::-1]):
   cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
 
 cv2.imwrite('final.jpg',img_rgb)
+
+f.close()
