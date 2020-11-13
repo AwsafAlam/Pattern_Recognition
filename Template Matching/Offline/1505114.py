fc243e10217eab58aa3fe50096bb9746dcc00045
Md Awsaf Alam <awsafalam@gmail.com>
2020-11-14 03:21:29 +0600

    Worked -- heiarchical search

--- Template Matching/Offline/1505114.py
+++ Template Matching/Offline/1505114.py
@@ -28,7 +28,6 @@ w, h = template.shape
 if not os.path.exists('out_frames'):
     os.makedirs('out_frames')
 
-
 def extract_frames():
   """
   Extracts video frames into images
@@ -95,12 +94,12 @@ def calculate_distance(img_gray,row = 0, col = 0):
 
   return int(sum)
 
-def template_match_2D_log(frame):
+def template_match_2D_log(frame, p):
   """
   2D Log
   """
   print("Starting 2D log ...")
-  global template,w,h,p, Xi, Xj
+  global template,w,h, Xi, Xj
   times_searched = 0
   
   # Read and convert both images to grayscale
@@ -117,56 +116,69 @@ def template_match_2D_log(frame):
   template = np.array(template)
   template = template.astype(np.int64)
 
-  # row_sums = template.sum(axis=1)
-  # template = template / row_sums[:, np.newaxis]
-
-  # row_sums = img_gray.sum(axis=1)
-  # img_gray = img_gray / row_sums[:, np.newaxis]
-
   print(img_gray.shape, template.shape)
   print("({},{}) - ({},{})".format(test_width,test_height,w,h))
   print(str(test_height - h), str(test_height), str(h))
-
+  print("Starting p = {}".format(p))
   min = INF
   centre_i, centre_j = 0,0
-  # start = (Xi - p, Xi+p)
-  # end = (Xj - p, Xj + p)
-  if method == 2:
-    p = p/2
+  
 
   if frame == 0:
     m_start, m_end = Xi,test_width - w + 1
     n_start, n_end = Xj,test_height - h + 1
-  else:
-    m_start, m_end = math.floor(Xi - p), math.floor(Xi+p)
-    n_start, n_end = math.floor(Xj - p), math.floor(Xj + p)
 
-  for i in range(m_start, m_end):
-    for j in range(n_start, n_end):
-      # calculating the mean distance
-      dist = calculate_distance(img_gray, i,j)
-      f.write("Coord: ({},{}) - Dist={}".format(i,j,dist))
-      times_searched = times_searched + 1
-      if(dist < min):
-        print("Found min ({})--- [{},{}]\n".format(dist,i,j))
-        min = dist
-        centre_i = i
-        centre_j = j
+    for i in range(m_start, m_end):
+      for j in range(n_start, n_end):
+        # calculating the mean distance
+        dist = calculate_distance(img_gray, i,j)
+        f.write("Coord: ({},{}) - Dist={}".format(i,j,dist))
+        times_searched = times_searched + 1
+        if(dist < min):
+          print("Found min ({})--- [{},{}]\n".format(dist,i,j))
+          min = dist
+          centre_i = i
+          centre_j = j
+  else:
+    
+    while True:
+      # find value of k and d
+      k = np.ceil(np.log2(p))
+      distance = int(np.power(2, k - 1))
+
+      # traverse over the bounded box
+      m_start, m_end = math.floor(Xi - distance), math.floor(Xi+ distance)
+      n_start, n_end = math.floor(Xj - distance), math.floor(Xj + distance)
+
+      for i in range(m_start, m_end):
+        for j in range(n_start, n_end):
+          # calculating the mean distance
+          dist = calculate_distance(img_gray, i,j)
+          f.write("Coord: ({},{}) - Dist={}".format(i,j,dist))
+          times_searched = times_searched + 1
+          if(dist < min):
+            print("Found min ({})--- [{},{}]\n".format(dist,i,j))
+            min = dist
+            centre_i = i
+            centre_j = j
+
+      Xi = centre_i
+      Xj = centre_j
+      p = p / 2.0
+
+      # when the distance is 1, break the while loop
+      if (distance == 1):
+          break
       
   print("done")
   print(centre_i,centre_j)
   if frame == 0:
     Xi = centre_i
     Xj = centre_j
+  
   # cv2.rectangle(img_rgb, (centre_i,centre_j), (centre_i + h, centre_j + w), (0,0,255), 2)
   cv2.rectangle(img_rgb, (centre_j,centre_i), (centre_j + h, centre_i + w), (0,0,255), 2)
   
-  # res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-  # threshold = 0.9
-  # loc = np.where( res >= threshold)
-
-  # for pt in zip(*loc[::-1]):
-  #   cv2.rectangle(img_rgb, pt, (pt[0] + h, pt[1] + w), (0,0,255), 2)
   cv2.imwrite(test_img,img_rgb)
   return times_searched
 
@@ -192,12 +204,6 @@ def template_match_Hierarchical(frame):
   template = np.array(template)
   template = template.astype(np.int64)
 
-  # row_sums = template.sum(axis=1)
-  # template = template / row_sums[:, np.newaxis]
-
-  # row_sums = img_gray.sum(axis=1)
-  # img_gray = img_gray / row_sums[:, np.newaxis]
-
   print(img_gray.shape, template.shape)
   print("({},{}) - ({},{})".format(test_width,test_height,w,h))
   print(str(test_height - h), str(test_height), str(h))
@@ -236,14 +242,9 @@ def template_match_Hierarchical(frame):
   # cv2.rectangle(img_rgb, (centre_i,centre_j), (centre_i + h, centre_j + w), (0,0,255), 2)
   cv2.rectangle(img_rgb, (centre_j,centre_i), (centre_j + h, centre_i + w), (0,0,255), 2)
   
-  # res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-  # threshold = 0.9
-  # loc = np.where( res >= threshold)
-
-  # for pt in zip(*loc[::-1]):
-  #   cv2.rectangle(img_rgb, pt, (pt[0] + h, pt[1] + w), (0,0,255), 2)
   cv2.imwrite(test_img,img_rgb)
   return times_searched
+
 
 def template_match_exhaustive(frame):
   """
@@ -335,7 +336,7 @@ if __name__ == "__main__":
   method = int(input())
   
   print("Enter the value of p")
-  p = int(input())
+  p = float(input())
 
   frames = extract_frames()
   print("Total Frames: {}".format(frames))
@@ -345,7 +346,7 @@ if __name__ == "__main__":
     if method == 3:
       performance = performance + template_match_Hierarchical(i)
     elif method == 2:
-      performance = performance + template_match_2D_log(i)
+      performance = performance + template_match_2D_log(i , p)
     else:
       performance = performance + template_match_exhaustive(i)
   
