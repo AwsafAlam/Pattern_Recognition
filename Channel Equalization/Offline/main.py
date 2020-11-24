f3cd515864c1451e234bd089e3ecaec6195d241f
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2020-11-24 23:06:20 +0600

    int list

--- Channel Equalization/Offline/main.py
+++ Channel Equalization/Offline/main.py
@@ -4,7 +4,25 @@ TEST_DATA="test.txt"
 TRAIN_DATA="train.txt"
 
 
+# Python code to convert string to list character-wise 
+def Convert(string): 
+    list1=[] 
+    list1[:0]=string 
+    return list1 
 
+
+file = open('train.txt', 'r')
+str = file.readline()
+# print(str)
+
+a_list=[] 
+a_list[:0]= str
+# a_list = str.split('')
+map_object = map(int, a_list)
+
+list_of_integers = list(map_object)
+
+print(list_of_integers) 
 
 # if __name__ == "__main__":
 #     print("Starting channel equalization")
